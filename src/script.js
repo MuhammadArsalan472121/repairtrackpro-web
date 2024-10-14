9a588d2bd525cd74017cf31dbc005c2ed5fdb2cd
Muhammad Arsalan <ak0256344@gmail.com>
2024-10-14 21:28:46 +0500

    basic forms structure is created

---
+++ src/script.js
@@ -0,0 +1,9 @@
+document.getElementById('customerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Customer form submitted!');
+});
+
+document.getElementById('productForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Product form submitted!');
+});
